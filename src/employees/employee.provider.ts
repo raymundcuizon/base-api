48f5a3c3f514056e9f217cf60df05efe6f4e1cf6
raymund <raymundcuizon07@gmail.com>
2023-11-15 21:05:04 +0800

    payroll items provider

--- src/employees/employee.provider.ts
+++ src/employees/employee.provider.ts
@@ -1,10 +1,23 @@
 import { DataSource } from 'typeorm';
 import { Employee } from './entities/employee.entity';
+import { PayrollItem } from './entities/payroll-items.entity';
+import { Payroll } from './entities/payroll.entity';
 
 export const employeeProviders = [
   {
     provide: 'EMPLOYEE_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(Employee),
+    inject: ['DATA_SOURCE'],
+  },
+  {
+    provide: 'PAYROLL_ITEM_REPOSITORY',
+    useFactory: (dataSource: DataSource) =>
+      dataSource.getRepository(PayrollItem),
+    inject: ['DATA_SOURCE'],
+  },
+  {
+    provide: 'PAYROLL_REPOSITORY',
+    useFactory: (dataSource: DataSource) => dataSource.getRepository(Payroll),
     inject: ['DATA_SOURCE'],
   },
 ];
